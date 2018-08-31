64b3e0b8feab760ac087ca218c672a702aae4456
Fabien Gréard <grd.fabien@gmail.com>
2018-08-31 16:06:45 +0200

    Update certbot-renew.sh

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # crontab -e to edit a crontab file
-# crontab task at 03:00 every day
-0 3 * * * cd /etc/letsencrypt/ && ./certbot-auto renew && cd /etc/tasks && ./restart
-# must reload the whole server thing
+# crontab task at 03:00 every day it use ./monit to restart PM2 instance replace your own restart system (appache, nodejs ...)
+0 3 * * * cd /etc/letsencrypt/ && ./certbot-auto renew && cd /etc/tasks && ./monit -r
+
