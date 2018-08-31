b911d85731f56cc568cbd793770f5bbaab9a1ff2
Fabien Gréard <grd.fabien@gmail.com>
2018-08-31 16:05:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,6 +40,9 @@ function install {
   # Rimraf
   sudo npm install rimraf -g
 
+  # Pm2
+  npm install pm2@latest -g
+
   # Allow npm compil install
   sudo apt install -y build-essential
 
@@ -61,6 +64,9 @@ function clear {
   sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
   sudo rm -rf /opt/local/bin/node /opt/local/include/node /opt/local/lib/node_modules
   sudo rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node.1 /usr/local/lib/dtrace/node.d
+
+  display "Delete hyper"
+  sudo apt-get purge --auto-remove -y hyper
 
   display "Done"
 }
