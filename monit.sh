4c455d24d9f05a53cdb6757e5856214691c3c460
Fabien Gréard <grd.fabien@gmail.com>
2018-08-31 16:04:58 +0200

    Create monit.sh

---
+++ monit.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+function helper {
+  echo "Usage: $0 [OPTIONS]"
+  echo ""
+  echo "OPTIONS:"
+  echo "-r, --restart Restart all processes"
+  echo "-s, --stop Stop all processes"
+}
+
+while true; do
+  case $1 in
+    -r | --restart)
+    sudo pm2 restart all
+    ;;
+    -s | --stop)
+    sudo pm2 stop all
+    ;;
+    *)
+    helper
+    ;;
+  esac
+  exit 0
+done
